6d9db8d19ace5866366473e2bf9f069ab1f3bcab
Ashraful Haque <vselfnet@gmail.com>
2023-06-13 02:20:49 +0200

    Course Detail Page Update

--- src/Pages/CourseDetail/CourseDetail.jsx
+++ src/Pages/CourseDetail/CourseDetail.jsx
@@ -1,9 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import useCourseDetail from "../../Hooks/useCourseDetail";
 import SectionTitle from "../../Components/SectionTitle";
+import './CourseDetail.css'
 
 const CourseDetail = () => {
     const [courses] = useCourseDetail();
+
     return (
         <div>
             <Helmet><title>Summer Camp | Course Detail</title></Helmet>
@@ -17,50 +19,41 @@ const CourseDetail = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Course Name</th>
+                            <th>Instructor</th>
+                            <th>Price</th>
+                            <th>Available Seeats</th>
+                            <th>Enrolled</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
+                        {
+                            courses.map((course, index) => <tr key={course._id} 
+                                className={`${course.seats === 0 ? 'bgRed' :  null}`}
+                            >
+                                <td>{index +1}</td>
+                                <td>
                                     <div className="avatar">
                                         <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            <img src={course.image} alt="Loading" />
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                            <td>Purple</td>
-                            <td>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </td>
-                        </tr>
+    
+                                </td>
+                                <td><div className="font-bold">{course.course}</div></td>
+                                <td>{course.instructor}</td>
+                                <td>{course.price}</td>
+                                <td>{course.seats}</td>
+                                <td>{course.enrolled}</td>
+                                <td><button className="btn btn-outline btn-xs" disabled={course.seats === 0 ? true: false}>Enroll</button></td>
+                            </tr> )
+                        }
+                        
                     </tbody>
-                   
+
 
                 </table>
             </div>
